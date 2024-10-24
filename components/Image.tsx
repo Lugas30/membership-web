22166d2b2733857a91582d45ad3a159aad12ada7
gibranfajar <gibransatritamaa@gmail.com>
2024-10-24 16:26:06 +0700

    update image

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export const Image = () => {
-  return <h1>Ini component Image</h1>;
+  return (
+    <h1>Image</h1>
+    <Image src={logo} alt="Logo" width={120} height={200} className="mx-auto" />
+  );
 };
